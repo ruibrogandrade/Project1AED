a034689c301f2c7d74e310f53d05a0d555f8fb1d
Andrekt02 <andremiguelpachecomorais@gmail.com>
2021-12-05 11:57:44 +0000

    ja roda

---
+++ Aeroporto/passageiros.cpp
@@ -0,0 +1,25 @@
+#include "passageiros.h"
+//#include "voo.h"
+
+Passageiro::Passageiro() {}
+
+Passageiro::Passageiro(string nome, vector<Bilhete> bilhetes) : Voo() {
+    this-> nome = nome;
+    this->bilhetes = bilhetes;
+}
+
+string Passageiro::getNome() {
+    return nome;
+}
+
+void Passageiro::addBilhete(string & nome, Bilhete b) {
+    Voo v; //como adicionar outro bilhete à mesma pessoa?
+    if (v.getVagas() != 0){
+        Passageiro p;
+        p.nome = nome;
+        bilhetes.push_back(b);
+        vagas--;
+    }
+
+
+}
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Rui/Desktop/AED_project1
+# Install script for directory: C:/Users/andre/CLionProjects/Projeto1AED
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/AED_project1")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/projeto1")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files (x86)/mingw-w64/i686-8.1.0-posix-dwarf-rt_v6-rev0/mingw32/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Rui/Desktop/AED_project1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/andre/CLionProjects/Projeto1AED/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
