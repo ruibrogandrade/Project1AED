02adc17edbf33c6563c50ba6e60c9c118d3e51ed
Andrekt02 <andremiguelpachecomorais@gmail.com>
2021-12-07 17:53:39 +0000

    remove erorr

--- Aeroporto/bagagens.cpp
+++ Aeroporto/bagagens.cpp
@@ -3,10 +3,10 @@
 gestaoBagagens::gestaoBagagens(unsigned int c, unsigned int n, unsigned int m) : numCarruagens(c), numPilhas(n), numMalas(m){}
 
 void gestaoBagagens::colocarMalas() {
-    if(tapete.size() > numCarruagens*numMalas*numPilhas ) throw "carro cheio";
-//?? avancar para a proxima cena quando ficar cheio?
+    if(tapete.size() > numCarruagens*numMalas*numPilhas ) throw "carrinho cheio";
+    int i, j;
     while(!tapete.empty()) {
-        while(carrinho.size() < numCarruagens) {
+        if(carrinho.size() < numCarruagens) {
             if(carruagem[i].size() < numPilhas) {
                 if(pilha[j].size() < numMalas) {
                     pilha[j].push(tapete.front())
