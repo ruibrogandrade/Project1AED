df73f5ce9ad474b4b7f6a7fc8a91e71f2a7e9953
Ruizaius <67200522+Ruizaius@users.noreply.github.com>
2021-12-14 17:53:15 +0000

    trying to improve main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "Aeroporto/Data.h"
+
+using namespace std;
+
+void checkTipoDeData() {
+    string TipoDeDataClasse;
+    cout << "What Type of Data do you want to Submit?" << endl;
+    cin >> TipoDeDataClasse;
+    if(TipoDeDataClasse == "Voo") {
+
+    }
+    if(TipoDeDataClasse == "Bagagem") {
+
+    }
+    if(TipoDeDataClasse == "Passageiro") {
+
+    }
+    if(TipoDeDataClasse == "Bilhete") {
+
+    }
+}
+
+
+void ReadFiles() {
+    char tiposervico;
+    Data data;
+    string nomeFuncionario;
+    ifstream infile("servicos.txt");
+    string line;
+    int i = 0;
+    while (getline(infile, line,','))
+    {
+        switch(i){
+            case(0):
+                tiposervico = line.c_str()[0];
+                i++;
+            case(1):
+
+        }
+
+    }
+}
+
 
 int main() {
 
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
