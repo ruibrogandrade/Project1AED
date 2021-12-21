b9cf342e674a67b2dde9cfeeafd0d062c54593d4
Rui Brogueira Andrade <up202007539@fe.up.pt>
2021-12-21 23:39:23 +0000

    final final push

--- main.cpp
+++ main.cpp
@@ -19,26 +19,6 @@ void ReadFiles() {
     Passageiro p;
     p.ReadBilhete();
 }
-/*
-void checkTipoDeData(queue<servico> servicosFeitos, queue<servico> servicosPorFazer) {
-    servico s;
-    string TipoDeDataClasse;
-    cout << "What Type of Data do you want to Submit?" << endl;
-    cin >> TipoDeDataClasse;
-    if(TipoDeDataClasse == "Voo") {
-
-    }
-    if(TipoDeDataClasse == "Bagagem") {
-
-    }
-    if(TipoDeDataClasse == "Passageiro") {
-
-    }
-    if(TipoDeDataClasse == "Servico") {
-        s.ServicoInput();
-    }
-}
-*/
 
 int MenuInicial() {
     int resposta;
